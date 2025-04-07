9b1e59c9799d317d523a70490d12239929df8750
daniel-fors <daniel.lennart.fors@gmail.com>
2025-04-07 14:08:07 +0200

    camera follow new player

--- Assets/Scripts/Controller/CameraFollow.cs
+++ Assets/Scripts/Controller/CameraFollow.cs
@@ -4,21 +4,26 @@ public class CameraFollow : MonoBehaviour
 {
     public float ZoomLevel = 12;
     public float WeZoomin = 20;
-    public float OffsetY = 2;
-    public float SmoothSpeed = 2.0f;
-    public float VerticalUpSmooth = 0.4f;
-    public float VerticalDownSmooth = 0.6f;
+    public float OffsetY = 1;
+    public float SmoothSpeed = 0.5f;
+    public float VerticalUpSmooth = 0.3f;
+    public float VerticalDownSmooth = 0.4f;
     public float MaxDistanceFromTarget = 6;
 
+    private Vector2 cameraPos;
+
+    private void Start()
+    {
+        cameraPos = (Vector2)GameLoop.Player.transform.position + Vector2.up * OffsetY;
+    }
 
     void LateUpdate()
     {
-        PlayerController player = GameLoop.Player;
-        Vector2 cameraPos = (Vector2)transform.position;
+        Player player = GameLoop.Player;
 
         Vector2 targetPos = (Vector2)player.transform.position + Vector2.up * OffsetY;
-        float verticalSmooth = player.velocity.y > 0 ? VerticalUpSmooth : VerticalDownSmooth;
-        Vector2 smoothVelocity = new Vector2(player.velocity.x, player.velocity.y * verticalSmooth);
+        float verticalSmooth = player.Velocity.y > 0 ? VerticalUpSmooth : VerticalDownSmooth;
+        Vector2 smoothVelocity = new Vector2(player.Velocity.x, player.Velocity.y * verticalSmooth);
         targetPos += smoothVelocity;
 
         targetPos = (Vector2)transform.position + (targetPos - cameraPos) * SmoothSpeed * Time.deltaTime;
@@ -26,7 +31,7 @@ public class CameraFollow : MonoBehaviour
         deltaPos = Vector2.ClampMagnitude(deltaPos, MaxDistanceFromTarget);
         targetPos = (Vector2)player.transform.position + deltaPos;
 
-        float zoom = Mathf.Clamp01(3f - Mathf.Log(1 + player.velocity.magnitude));
+        float zoom = Mathf.Clamp01(3f - Mathf.Log(1 + player.Velocity.magnitude));
         zoom = Mathf.Lerp(WeZoomin, ZoomLevel, zoom);
         float currentZoom = Mathf.Abs(transform.position.z);
         float deltaZoom = (zoom - currentZoom) * SmoothSpeed * Time.deltaTime;
@@ -34,5 +39,6 @@ public class CameraFollow : MonoBehaviour
 
         Vector3 pos = new Vector3(targetPos.x, targetPos.y, -zoom);
         transform.position = pos;
+        cameraPos = pos;
     }
 }
